00266172d84b4ce20a4e28260f884ed19f457d3b
Harin Kaklotar <harinlive@gmail.com>
2023-09-09 17:09:13 +0530

    first commit

---
+++ src/main/java/com/harin/springpracticeone/SpringPracticeOneApplication.java
@@ -0,0 +1,13 @@
+package com.harin.springpracticeone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/harin/springpracticeone/SpringPracticeOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harin.springpracticeone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPracticeOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
