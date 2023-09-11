b07fe4f6014316b71290e3c27745c14a435d2336
Harin Kaklotar <harinlive@gmail.com>
2023-09-12 01:44:22 +0530

    implement try with resources

--- src/main/java/com/harin/springpracticeone/bean/HelloWorldSpring.java
+++ src/main/java/com/harin/springpracticeone/bean/HelloWorldSpring.java
@@ -2,29 +2,33 @@ package com.harin.springpracticeone.bean;
 
 import java.util.Arrays;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class HelloWorldSpring {
 
 	public static void main(String[] args) {
 		
-		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
-		System.out.println(context.getBean("name"));
-		System.out.println(context.getBean("age"));
-		System.out.println(context.getBean("person"));
-		System.out.println(context.getBean("address"));
-		
-		System.out.println(context.getBean("CustomNameAddress"));
-		System.out.println(context.getBean("personByMethod"));
-		System.out.println(context.getBean("personByParameter"));
-		System.out.println(context.getBean("student"));
-		System.out.println(context.getBean("student2"));
-		System.out.println(context.getBean(Person.class));
-		System.out.println(context.getBean(Address.class));
-		System.out.println(context.getBean(Student.class));
-		System.out.println(context.getBean("primaryStudentByQualifire"));
-		
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		try (var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class)) {
+			System.out.println(context.getBean("name"));
+			System.out.println(context.getBean("age"));
+			System.out.println(context.getBean("person"));
+			System.out.println(context.getBean("address"));
+			
+			System.out.println(context.getBean("CustomNameAddress"));
+			System.out.println(context.getBean("personByMethod"));
+			System.out.println(context.getBean("personByParameter"));
+			System.out.println(context.getBean("student"));
+			System.out.println(context.getBean("student2"));
+			System.out.println(context.getBean(Person.class));
+			System.out.println(context.getBean(Address.class));
+			System.out.println(context.getBean(Student.class));
+			System.out.println(context.getBean("primaryStudentByQualifire"));
+			
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+		} catch (BeansException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
