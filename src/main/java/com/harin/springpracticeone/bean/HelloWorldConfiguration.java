bcedef3ef873de60ba7e4add63211478b8590d44
Harin Kaklotar <harinlive@gmail.com>
2023-09-12 00:14:37 +0530

    more example of beans

--- src/main/java/com/harin/springpracticeone/bean/HelloWorldConfiguration.java
+++ src/main/java/com/harin/springpracticeone/bean/HelloWorldConfiguration.java
@@ -11,4 +11,22 @@ public class HelloWorldConfiguration {
 		return "Hello world!";
 	}
 
+	
+	@Bean
+	public int age() {
+		return 10;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Harin", 16);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("Andheri", "Mumbai");
+	}
 }
+
+record Person(String name, int age) {};
+record Address(String firstLine, String city) {};
